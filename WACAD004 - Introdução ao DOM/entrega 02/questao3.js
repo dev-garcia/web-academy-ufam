0b6253b0acaa5c5d800fdf1209238eee2210c0cf
Cristian Garcia <garcia.ac.cristian@gmail.com>
2024-08-27 20:11:14 -0400

    feat: adiciona a resolução da questão 3 do laboratorio 02 do modulo Introdução ao DOM.

---
+++ WACAD004 - Introdução ao DOM/entrega 02/questao3.js
@@ -0,0 +1,7 @@
+const lista = document.getElementById("lista-itens-1");
+
+const novoItemLista = document.createElement("li");
+
+novoItemLista.textContent = "Novo Item";
+
+lista.appendChild(novoItemLista);
