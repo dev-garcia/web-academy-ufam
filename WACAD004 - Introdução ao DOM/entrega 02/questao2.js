360856b6e1d4021e3c19625bea7f796bbd8c5678
Cristian Garcia <garcia.ac.cristian@gmail.com>
2024-08-27 20:07:19 -0400

    feat: adiciona a resolução da questão 2 do laboratorio 02 do modulo Introdução ao DOM.

---
+++ WACAD004 - Introdução ao DOM/entrega 02/questao2.js
@@ -0,0 +1,5 @@
+const listaTitulo = document.getElementById("lista-titulo");
+listaTitulo.firstChild.nodeValue = "Nova Lista de Itens";
+
+const listaItemDois = document.getElementById("two");
+listaItemDois.textContent = "Item B Atualizado";
